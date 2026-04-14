4503339c6b8927f6a3b924d5c271d783b2bcd608
Priyanshu Kumar <priyanshucpr22@gmail.com>
2026-04-14 23:29:58 +0530

    Time: 6 ms (46.53%), Space: 46.3 MB (28.55%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canTransform(String start, String result) {
+        
+        if (!start.replace("X", "").equals(result.replace("X", ""))) {
+            return false;
+        }
+        
+        int i = 0, j = 0;
+        int n = start.length();
+        
+        while (i < n && j < n) {
+            
+            while (i < n && start.charAt(i) == 'X') i++;
+            while (j < n && result.charAt(j) == 'X') j++;
+            
+            if (i == n && j == n) return true;
+            
+            char c = start.charAt(i);
+            
+            if (c == 'L' && i < j) return false;
+            if (c == 'R' && i > j) return false;
+            
+            i++;
+            j++;
+        }
+        
+        return true;
+    }
+}
