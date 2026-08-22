6ae4c8bdbdd1a1cd68969b4384598da47bc59e49
Priyanshu Kumar <priyanshucpr22@gmail.com>
2026-08-22 11:37:53 +0530

    Time: 14 ms (91.17%), Space: 46.4 MB (52.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,8 +1,12 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+
         int[] dp = new int[amount + 1];
 
-        Arrays.fill(dp, amount + 1);
+        for (int i = 0; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
+
         dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
@@ -13,6 +17,10 @@ class Solution {
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        if (dp[amount] == amount + 1) {
+            return -1;
+        }
+
+        return dp[amount];
     }
 }
